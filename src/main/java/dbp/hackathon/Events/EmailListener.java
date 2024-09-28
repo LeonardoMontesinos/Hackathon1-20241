101e79c576a58005f27e5ddf7e3925e74e229c9f
Valentina Alvarez <valentina.alvarez@utec.edu.pe>
2024-09-28 17:36:15 -0500

    email html

--- src/main/java/dbp/hackathon/Events/EmailListener.java
+++ src/main/java/dbp/hackathon/Events/EmailListener.java
@@ -1,9 +1,13 @@
 package dbp.hackathon.Events;
 
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class EmailListener {
@@ -13,7 +17,19 @@ public class EmailListener {
     @EventListener
     @Async
     public void handleHelloEmailEvent(HelloEmailEvent event){
-        emailService.sendMessage(event.getEmail(), "hola", "hola");
+        try{
+            Map<String, Object> templateData = new HashMap<>();
+            templateData.put("nombre", "Juan Perez");
+            templateData.put("nombrePelicula", "El");
+            templateData.put("fecha", "septiembre");
+            templateData.put("Cantidad de entradas", 3);
+            templateData.put("precio total", "30");
+            templateData.put("qr", " " );
 
+            emailService.sendHtml(event.getEmail(), "Gracias por tu compra!", templateData);
+
+        }  catch (MessagingException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/dbp/hackathon/Events/EmailService.java
+++ src/main/java/dbp/hackathon/Events/EmailService.java
@@ -1,26 +1,46 @@
 package dbp.hackathon.Events;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+import java.util.Map;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender EmailSender;
+    private SpringTemplateEngine templateEngine;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.EmailSender = mailSender;
+    @Autowired
+    public EmailService(JavaMailSender emailSender, SpringTemplateEngine templateEngine){
+        this.EmailSender=emailSender;
+        this.templateEngine=templateEngine;
+    }
+    public void sendHtml(String to, String subject, Map<String,Object> templateData) throws MessagingException{
+        MimeMessage mimeMessage = EmailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        Context context = new Context();
+        context.setVariables(templateData);
+        String htmlContent = templateEngine.process("emailTemplate",context);
+
+        helper.setText(htmlContent,true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setFrom("valentina.alvarez@utec.edu.pe");
+
+        EmailSender.send(mimeMessage);
+
+
     }
 
 
-    public void sendMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        EmailSender.send(message);
-    }
 
 }
