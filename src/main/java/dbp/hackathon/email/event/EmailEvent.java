bb719708312f20a9e7902c02b79d304928838067
Alvaro <alvaro.tapia@utec.edu.pe>
2024-09-28 16:34:25 -0500

    Event Email

--- src/main/java/dbp/hackathon/email/event/EmailEvent.java
+++ src/main/java/dbp/hackathon/email/event/EmailEvent.java
@@ -1,4 +1,16 @@
 package dbp.hackathon.email.event;
+import org.springframework.context.ApplicationEvent;
 
-public class EmailEvent {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailEvent extends ApplicationEvent {
+    private String email;
+
+    public EmailEvent(Object source, String email) {
+        super(source);
+        this.email = email;
+    }
 }
--- src/main/java/dbp/hackathon/email/event/EmailListener.java
+++ src/main/java/dbp/hackathon/email/event/EmailListener.java
@@ -1,4 +1,23 @@
 package dbp.hackathon.email.event;
 
 public class EmailListener {
+    public void onEmailReceived(EmailEvent emailEvent) {
+        System.out.println("Email received: " + emailEvent.getEmail());
+    }
+
+    public void onEmailSent(EmailEvent emailEvent) {
+        System.out.println("Email sent: " + emailEvent.getEmail());
+    }
+
+    public void onEmailDeleted(EmailEvent emailEvent) {
+        System.out.println("Email deleted: " + emailEvent.getEmail());
+    }
+
+    public void onEmailRead(EmailEvent emailEvent) {
+        System.out.println("Email read: " + emailEvent.getEmail());
+    }
+
+    public void onEmailUnread(EmailEvent emailEvent) {
+        System.out.println("Email unread: " + emailEvent.getEmail());
+    }
 }
